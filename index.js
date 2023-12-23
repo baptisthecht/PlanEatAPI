81be75315d1450bea8a738d7ad64099788c7d848
Baptist HECHT <me@baptisthecht.fr>
2023-12-23 22:00:28 +0100

    Beautify

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from "express";
-const app = express();
 import cors from "cors";
+
+// Create Express app
+const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Create testing route
 app.get("/health", (req, res) => {
 	res.send("Life is beautiful!");
 });
 
+// Launching the app on port 8800
 app.listen(8800, () => {
 	console.log("API Working!");
 });
